886de487a333b3a31e1fa81381e2fe85d14a1e32
Pedro Henrique <phenriqueangelodesouza@gmail.com>
2026-08-31 19:18:29 -0300

    adding packages for code splitting, establishing the connection, and structuring environment variables.

---
+++ Back-end/JDBC/aleluia/src/main/java/connection/DataBaseConnection.java
@@ -0,0 +1,41 @@
+package connection;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+
+public class DataBaseConnection {// classe que vai referenciar minha conexão com o banco
+
+    //ATRIBUTOS
+    private static DataBaseConnection instance;
+    private Connection connection;
+
+    //construtor privado
+    private DataBaseConnection() {
+        Dotenv dotenv = Dotenv.load();
+        String url = dotenv.get("DB_URL");
+        String user = dotenv.get("DB_USER");
+        String password = dotenv.get("DB_PASSWORD");
+
+
+        try { //abrindo a conexão com o banco, dentro do try para que a aplicação não quebre
+            connection = DriverManager.getConnection(url, user, password);
+        }catch (SQLException sqlE){
+            throw new RuntimeException ("Ocorereu um erro");
+        }
+    }
+    //unico metodo de entrada para a conexao com o banco
+    public static DataBaseConnection getInstance() {
+        if (Objects.isNull(instance)){
+            instance = new DataBaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection connection(){ //metodo responsavel pelo retorno da conexão
+        return connection;
+    }
+}
---
+++ Back-end/JDBC/aleluia/src/main/java/model/User.java
@@ -0,0 +1,102 @@
+package model;
+
+public class User {
+    //ATRIBUTOS
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+    private String registrationDate;
+    private String lastAcces;
+    private String status;
+    private String position;
+    private Employee employee;
+    //CONSTRUTOR
+    public User(String userName, String email, String password, Employee employee, String position) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.employee = employee;
+        this.position = position;
+    }
+    //SOBRECARGA
+    public User(int id, String userName, String email, String password, String registrationDate, String lastAcces, String status, String position, Employee employee) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.registrationDate = registrationDate;
+        this.lastAcces = lastAcces;
+        this.status = status;
+        this.position = position;
+        this.employee = employee;
+    }
+    //GETTERS
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public String getLastAcces() {
+        return lastAcces;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+    //SETTERS
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public void setLastAcces(String lastAcces) {
+        this.lastAcces = lastAcces;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+}
